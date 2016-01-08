b3a8fbdce21585279995f50813e25c3b67788aa6
daison8383 <faceless4221@gmail.com>
2016-01-08 17:33:59 +0900

    fix:invalid user destroy

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -55,10 +55,14 @@ class ImagesController < ApplicationController
   # DELETE /images/1
   # DELETE /images/1.json
   def destroy
-    @image.destroy
-    respond_to do |format|
-      format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
-      format.json { head :no_content }
+    if @image.user == @current_user
+        @image.destroy
+        respond_to do |format|
+          format.html { redirect_to images_url, notice: 'Image was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+    else
+        redirect_to images_path, notice: 'faild'
     end
   end
 
