740548854f7fc7f461345ad5051070e279af338e
daison8383 <faceless4221@gmail.com>
2016-01-13 17:23:17 +0900

    fix: invaild user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,15 +41,19 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      if @user.id == @current_user.id
+        respond_to do |format|
+          if @user.update(user_params)
+            format.html { redirect_to @user, notice: 'User was successfully updated.' }
+            format.json { render :show, status: :ok, location: @user }
+          else
+            format.html { render :edit }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+        end
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        redirect_to users_url , notice: 'faild'
       end
-    end
   end
 
   # DELETE /users/1
